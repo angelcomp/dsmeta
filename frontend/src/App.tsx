e969d6272108ccff7509f04149db534bc764aef4
Angelica dos Santos <santosangelicassp@gmail.com>
2022-11-19 00:20:21 -0300

    card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
