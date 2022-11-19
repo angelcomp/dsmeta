73ba48b382efe9be2a123064546fcedf627c4dae
Angelica dos Santos <santosangelicassp@gmail.com>
2022-11-19 00:07:05 -0300

    header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return(
     <>
-      <h1>Hello World!</h1>
-      <NotificationButton/>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="meta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/img/meta-logo.svg"
+import "./styles.css"
+
+function Header() {
+    return (
+        <header>
+            <div className="meta-logo-container">
+                <img src={logo} alt="Meta Logo" />
+                <h1>Meta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/angelcomp?tab=repositories">angelcomp</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
